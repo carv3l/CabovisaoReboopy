c32993ea5da2fcab19327871d0cbba755f60c180
carv3l <carv3l@vivaldi.net>
2022-07-11 22:57:55 +0100

    Removed useless libraries added by accident

--- scripty.py
+++ scripty.py
@@ -1,6 +1,5 @@
-from tkinter import SW
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.keys import Keys
 import datetime
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
